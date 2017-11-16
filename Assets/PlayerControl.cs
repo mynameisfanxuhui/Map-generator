48aa6a9664fbf3f2f480d93a8c0d1444f55b26c5
Torres Fan <torres.x.fan@pwc.com>
2017-11-16 11:16:04 +0800

    initial

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private Rigidbody rig;
+    private Vector3 velocity;
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized*10;
+    }
+    private void FixedUpdate()
+    {
+        //rig.velocity = velocity * 10;
+        rig.MovePosition(rig.position + velocity * Time.fixedDeltaTime);
+    }
+}
